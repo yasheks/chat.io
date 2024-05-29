d931202dc32bd651b973b91310a15295f90f1548
yasheks <yakovzeld@gmail.com>
2024-05-29 16:37:56 +0300

    beta

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,28 @@
-const socket = io()
-const messages = document.querySelector(".messages")
-const form = document.querySelector(".form")
-const input = document.querySelector(".input")
-const nameblock = document.querySelector(".name")
+const socket = io();
 
-const userName = prompt("Ваше имя: ")
-nameblock.innerHTML = `${userName}`
+const messages = document.querySelector(".messages");
+const form = document.querySelector(".form");
+const input = document.querySelector(".input");
+const nameblock = document.querySelector(".name");
+
+const userName = prompt("Ваше имя: ");
+nameblock.innerHTML = `${userName}`;
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if(input.value){
+        socket.emit("chat message", {
+            message: input.value,
+            name: userName
+        });
+        input.value = '';
+    }
+});
+
+socket.on("chat message", (data) =>{
+    const item = document.createElement("li");
+    item.innerHTML = `<span>${data.name}</span>: ${data.message}`;
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight)
+});
--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const express = require("express")
-const app = express()
-const http = require("http").createServer(app)
-const io = require("socket.io")(http)
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html")
-})
-app.use(express.static(__dirname + "/assets"))
+    res.sendFile(__dirname + "/index.html");
+});
+app.use(express.static(__dirname + "/assets"));
 
+io.on("connection", (socket) => {
+    socket.on("chat message", (data) => {
+        io.emit("chat message", {
+            message: data.message,
+            name: data.name,
+        });
+    });
+});
 
 http.listen(3000, () => {
-    console.log("сервер запущен")
-})
+    console.log("сервер запущен");
+});
